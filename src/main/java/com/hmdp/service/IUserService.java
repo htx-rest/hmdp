09e3ecb5f9d560583b8074087b2d8e070ec11f7d
xht <1656855152@qq.com>
2023-08-30 21:17:39 +0800

    用户签到

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -15,4 +15,8 @@ public interface IUserService extends IService<User> {
     Result sendCode(String phone, HttpSession session);
 
     Result login(LoginFormDTO loginForm, HttpSession session);
+
+    Result sign();
+
+    Result signCount();
 }
